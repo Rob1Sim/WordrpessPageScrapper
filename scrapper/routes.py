eb5221bd579d4594e4f82b248f6a1af798c230e3
Robin Simonneau <robin.le.dev@gmail.com>
2023-06-01 11:59:13 +0200

    Ajout du listing des toutes les pages dans le /

--- scrapper/routes.py
+++ scrapper/routes.py
@@ -25,7 +25,11 @@ for file in html_files:
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('index.html')
+    html_files = [file for file in template_files if file.endswith('.html')]
+    link_list = ""
+    for file in html_files:
+        link_list += "<a href='"+path.splitext(file)[0]+"'>"+path.splitext(file)[0]+"</a>"
+    return link_list
 
 
 @app.route("/scrap")
