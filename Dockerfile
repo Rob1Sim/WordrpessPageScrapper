f459e41d1c19ebb7e2b868dcab11ad2f3a27b78c
Robin Simonneau <robin.le.dev@gmail.com>
2023-06-02 14:45:51 +0200

    Ajout du redémarage du serveur tous les soirs à 2h du matins pour pouvoir indexé les nouvelles pages

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,16 @@
     FROM python:3.10-alpine3.18
     LABEL authors="Robin Simonneau"
+
+    # Installation de cron
+    RUN apk update && apk add --no-cache dcron
     COPY . /app
     WORKDIR /app
     RUN pip install -r requirements.txt
-    EXPOSE 5000
-    CMD ["python", "app.py"]
+    # Ajout de la tâche planifiée dans cron
+    RUN echo "6       12       *       *       *       pkill -f app.py && sleep 5 && python app.py &" >> /etc/crontabs/root
     ENV CONFIG_FILE=/app/scrapper/.env
+    EXPOSE 5000
+    COPY entrypoint.sh /app/entrypoint.sh
+    RUN chmod +x /app/entrypoint.sh
+    ENTRYPOINT ["/app/entrypoint.sh"]
     VOLUME /app/scrapper/templates
---
+++ entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Fonction pour démarrer le script Python
+start_script() {
+    echo "Démarrage du script Python..."
+    python app.py > /dev/null 2>&1 &
+    APP_PID=$!
+    echo "Processus Python démarré. PID : $APP_PID"
+}
+
+# Fonction pour arrêter le script Python
+stop_script() {
+    echo "Arrêt du script Python..."
+    kill $APP_PID
+    wait $APP_PID
+    echo "Processus Python arrêté."
+}
+
+# Vérification de l'heure de redémarrage
+check_restart_time() {
+    restart_hour="22"
+    restart_minute="00"
+
+    current_hour=$(date +%H)
+    current_minute=$(date +%M)
+
+    if [ "$current_hour" = "$restart_hour" ] && [ "$current_minute" = "$restart_minute" ]; then
+        echo "Heure de redémarrage atteinte. Arrêt du script Python en cours..."
+        stop_script
+        start_script
+    fi
+}
+
+# Démarrage initial du script Python
+start_script
+
+# Boucle infinie pour vérifier l'heure de redémarrage toutes les minutes
+while true; do
+    sleep 60
+    check_restart_time
+done
