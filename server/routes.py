97835717b06c018e2c3d9e4efd1dd59bc7d17777
Robin Simonneau <robin.le.dev@gmail.com>
2023-06-05 10:35:45 +0200

    Correction suppression du static de index

--- server/routes.py
+++ server/routes.py
@@ -7,12 +7,12 @@ from os import listdir, path
 
 
 @app.route('/')
-def hello_world():
+def index():
     template_files = listdir('server/templates')
     html_files = [file for file in template_files if file.endswith('.html')]
     link_list = ""
     for file in html_files:
-        link_list += "<a href='/static/" + path.splitext(file)[0] + "'>" + path.splitext(file)[0] + "</a>"
+        link_list += "<a href='" + path.splitext(file)[0] + "'>" + path.splitext(file)[0] + "</a>"
     routes = [str(rule) for rule in app.url_map.iter_rules()]
 
     # Affichage des routes
