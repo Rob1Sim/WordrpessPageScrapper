01591b9b730c6086cf95b6f616757bb9e2fd22c7
Robin Simonneau <robin.le.dev@gmail.com>
2023-06-05 10:36:11 +0200

    Suppression des prints

--- server/routes.py
+++ server/routes.py
@@ -13,11 +13,7 @@ def index():
     link_list = ""
     for file in html_files:
         link_list += "<a href='" + path.splitext(file)[0] + "'>" + path.splitext(file)[0] + "</a>"
-    routes = [str(rule) for rule in app.url_map.iter_rules()]
 
-    # Affichage des routes
-    for route in routes:
-        print(route)
     return link_list
 
 
